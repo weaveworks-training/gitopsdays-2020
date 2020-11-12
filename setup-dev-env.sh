53edb03e5f29fe087858d4dc1f00fea3b87168a3
Brice Fernandes <brice@weave.works>
2020-11-12 22:46:28 +0000

    remove extra colours

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -55,30 +55,14 @@ echo ". <(flux completion bash)" >> ~/.bashrc
 # Clean up the console
 #
 
-cat <<END >> ~/.bashrc
-B_BLACK="\[\e[40m\]"
-B_RED="\[\e[41m\]"
-B_GREEN="\[\e[42m\]"
-B_YELLOW="\[\e[43m\]"
-B_BLUE="\[\e[44m\]"
-B_MAGENTA="\[\e[45m\]"
-B_CYAN="\[\e[46m\]"
-B_WHITE="\[\e[47m\]"
+cat << "END" >> ~/.bashrc
 
-F_BLACK="\[\e[30m\]"
-F_RED="\[\e[31m\]"
 F_GREEN="\[\e[32m\]"
-F_YELLOW="\[\e[33m\]"
-F_BLUE="\[\e[34m\]"
-F_MAGENTA="\[\e[35m\]"
 F_CYAN="\[\e[36m\]"
-F_WHITE="\[\e[37m\]"
-
 BOLD="\[\e[1m\]"
-
 DONE="\[\e[0m\]"
 
-export PS1="${F_GREEN}${BOLD}Gitops Days 2020 (${DONE}${F_CYAN}\W${DONE}${F_GREEN}${BOLD})➤ ${DONE}"
+export PS1="${F_GREEN}${BOLD}Gitops Days 2020 (${DONE}${F_CYAN}\w${DONE}${F_GREEN}${BOLD})➤ ${DONE}"
 
 END
 
