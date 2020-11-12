819d7da0eda831054c4d3ba43f8fef293283aacb
Brice Fernandes <brice@weave.works>
2020-11-12 22:03:30 +0000

    Add welcome message

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -1,10 +1,33 @@
 #! /usr/bin/bash
 
+echo <<END
+####### GITOPS DAYS 2020 TRAINING ####################
+
+Hi There!
+
+Welcome to the GitOps days 2020 workshop on Getting
+Started with GitOps!
+
+You can find all required resources, as well as step
+by step instructions for this workshop online at
+
+https://github.com/weaveworks-training/gitopsdays-2020
+
+Hope you enjoy the workshop and don't hesitate to ask
+plenty of questions.
+
+Happy GitOpsing :)
+
+Brice
+
+######################################################
+END
+
 
 #
 # Install Pulumi
 #
-curl -fsSL https://get.pulumi.com | sh
+curl -fsSL https://get.pulumi.com | sudo sh
 export PATH=$PATH:/home/training/.pulumi/bin
 
 
